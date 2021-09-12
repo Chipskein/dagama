db2ff14481f027976fecb13d00b53c768140a057
Chipskein <abfn0905@gmail.com>
2021-09-12 14:18:59 -0300

    sale

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="imgs/icon.png" type="image/jpg">
-    <title>Register</title>
+    <title>dagama</title>
 </head>
 <body>
   <main>    
