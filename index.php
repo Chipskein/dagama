6851c4c3285e0141584a69f0a832d5a74cdf575b
Chipskein <abfn0905@gmail.com>
2021-09-12 13:29:26 -0300

    safas

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
   <script>
       const url='https://dagama.herokuapp.com'
       //const url='localhost:8080'
-      const button=document.getElementsByid('register').onclick=()=>{
+      const button=document.getElementsById('register').onclick=()=>{
         window.location.href(`${url}/register.php`)
       }
   </script>
