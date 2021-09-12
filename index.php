dbfe444ec21f704cc1dc2a795809ffc94e984efc
Chipskein <abfn0905@gmail.com>
2021-09-12 14:07:40 -0300

    safas

--- index.php
+++ index.php
@@ -9,21 +9,22 @@
 </head>
 <body>
   <main>    
-    <form action="./mar.php" method="POST">
-      <label for="email">Email: <input type="email" required></label>
+    <form action="mar.php" method="POST">
+      <label for="email">Email: <input type="email" id='email' name='email' required></label>
       <br>
-      <label for="password">Password:<input type="password" required></label>
+      <label for="password">Password:<input type="password" id='password' name='password'required></label>
       <br>
-      <button id='login' type="submit">login</button>
+      <button id='login' name='submit' type="submit">login</button>
+      <button id='register'>registrar</button>
     </form>
+    
   </main>
   <script>
       const url='https://dagama.herokuapp.com';
       //const url='localhost:8080';
-      //<button id='register'>registrar</button>
-      //const button=document.getElementById('register').onclick=()=>{
-        //window.location.href=`${url}/register.php`;
-      //};
+      const button=document.getElementById('register').onclick=()=>{
+       window.location.href=`${url}/register.php`;
+      };
   </script>
 </body>
 </html>
