fff8f24047b61e28e3d1f04397f34514c631040e
Chipskein <abfn0905@gmail.com>
2021-10-23 02:23:03 -0300

    testando sqlite

--- mar.php
+++ mar.php
@@ -1,8 +1,9 @@
 <?php
-    $db=new SQLite3("face.db");
-    $test=$db->query("select * from pessoa")->fetchArray();
-    while($row=$test){
-        var_dump($row);
-    }
+    //$db=new SQLite3("face.db");
+    //$test=$db->query("select * from pessoa")->fetchArray();
+    //while($row=$test){
+      //  var_dump($row);
+    //}
+    //$db->close();
 
 ?>
