a0b3a7b87cb4b733e5a62a1a3dcd68da65819b8f
Chipskein <abfn0905@gmail.com>
2021-11-03 19:47:01 -0300

    testing connection

--- backend/infra/connection.php
+++ backend/infra/connection.php
@@ -2,9 +2,17 @@
     //connect with SQLite
     if(preg_match("/localhost/","$_SERVER[HTTP_HOST]")){
         echo "Connect with sqlite<br>";
+        $db=new SQLite3("");
+        $db->exec("");
     }
     if(preg_match("/dagama.herokuapp/","$_SERVER[HTTP_HOST]")){
         echo "Connect with postgresql<br>";
+        $url=getenv("$DATABASE_URL");
+        echo $url;
+        //$db=pg_connect("");
+    }
+    if(!preg_match("/localhost/","$_SERVER[HTTP_HOST]")&&!preg_match("/dagama.herokuapp/","$_SERVER[HTTP_HOST]")){
+        echo "<h2>Inválido</h2>";
     }
     
 ?>
