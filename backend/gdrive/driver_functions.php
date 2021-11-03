8cf44cdaf6f371dec9ebc6bc3de788e98e8192d1
Chipskein <abfn0905@gmail.com>
2021-11-03 18:57:02 -0300

    .

--- backend/gdrive/driver_functions.php
+++ backend/gdrive/driver_functions.php
@@ -1,70 +1,68 @@
 <?php
-require '../../vendor/autoload.php';
-function getClient(){
-    $client = new Google\Client();
-    $client->setApplicationName("dagama-gdrive");
-    $client->setAuthConfig('credentials.json');
-    $client->addScope('https://www.googleapis.com/auth/drive');
-    $client->addScope('https://www.googleapis.com/auth/drive.file');
-    $client->addScope('https://www.googleapis.com/auth/drive.readonly');
-    $client->addScope('https://www.googleapis.com/auth/drive.metadata.readonly');
-    $client->addScope('https://www.googleapis.com/auth/drive.metadata');
-    $client->addScope('https://www.googleapis.com/auth/drive.photos.readonly');
-    return $client;
-}
-function getAllfiles() {
-    $service=new Google\Service\Drive(getClient());
-    $optParams = array(
-        'pageSize' => 10,
-        'fields' => 'nextPageToken, files(id, name)'
-      );
-      $results = $service->files->listFiles($optParams);
+  require_once '../../vendor/autoload.php';
+  $FOLDERS=array("root"=>"14oQWzTorITdqsK7IiFwfTYs91Gh_NcjS","avatares"=>"1Z3A4iqIe1eMerkdTEkXnjApRPupaPq-M","portos"=>"1e5T21RxDQ-4Kqw8EDVUBICGPeGIRSNHx");
+  function getClient(){
+      $client = new Google\Client();
+      $client->setApplicationName("dagama-gdrive");
+      $client->setAuthConfig('credentials.json');
+      $client->addScope('https://www.googleapis.com/auth/drive');
+      $client->addScope('https://www.googleapis.com/auth/drive.file');
+      $client->addScope('https://www.googleapis.com/auth/drive.readonly');
+      $client->addScope('https://www.googleapis.com/auth/drive.metadata.readonly');
+      $client->addScope('https://www.googleapis.com/auth/drive.metadata');
+      $client->addScope('https://www.googleapis.com/auth/drive.photos.readonly');
+      return $client;
+  }
+  function getAllfiles() {
+      $service=new Google\Service\Drive(getClient());
+      $optParams = array(
+          'pageSize' => 10,
+          'fields' => 'nextPageToken, files(id, name)'
+        );
+        $results = $service->files->listFiles($optParams);
+        
+        if (count($results->getFiles()) == 0) {
+            print "No files found.\n";
+        } else {
+            print "Files:\n";
+            foreach ($results->getFiles() as $file) {
+                printf("%s (%s)\n", $file->getName(), $file->getId());
+            }
+        }
+  };
+  //folders
+  function insertFile($mimeType, $filename,$folderID,$newfilename) {
+      $client=getClient();
+      $service=new Google\Service\Drive($client);
       
-      if (count($results->getFiles()) == 0) {
-          print "No files found.\n";
-      } else {
-          print "Files:\n";
-          foreach ($results->getFiles() as $file) {
-              printf("%s (%s)\n", $file->getName(), $file->getId());
-          }
-      }
-};
-//folders
-$FOLDERS=array("root"=>"14oQWzTorITdqsK7IiFwfTYs91Gh_NcjS","avatares"=>"1Z3A4iqIe1eMerkdTEkXnjApRPupaPq-M","portos"=>"1e5T21RxDQ-4Kqw8EDVUBICGPeGIRSNHx");
-$gdrive_root="14oQWzTorITdqsK7IiFwfTYs91Gh_NcjS";
-$gdrive_avatar="1Z3A4iqIe1eMerkdTEkXnjApRPupaPq-M";
-function insertFile($mimeType, $filename,$folderID,$newfilename) {
-    $client=getClient();
-    $service=new Google\Service\Drive($client);
+      $file = new Google\Service\Drive\DriveFile();
+      $file->setMimeType($mimeType);
+      //set to user id
+      $file->setName($newfilename);
+      $file->setParents(array($folderID));
+      try {
+        $data = file_get_contents($filename);
+        $createdFile = $service->files->create($file, array(
+          'data' => $data,
+          'mimeType' => $mimeType,
+        ));
     
-    $file = new Google\Service\Drive\DriveFile();
-    $file->setMimeType($mimeType);
-    //set to user id
-    $file->setName($newfilename);
-    $file->setParents(array($folderID));
-    try {
-      $data = file_get_contents($filename);
-      $createdFile = $service->files->create($file, array(
-        'data' => $data,
-        'mimeType' => $mimeType,
-      ));
-  
 
-      return $createdFile;
-    } catch (Exception $e) {
-      print "An error occurred: " . $e->getMessage();
-    }
-}
-
-if(isset($_FILES["foto"])){
-  $file=$_FILES["foto"];
-  $server_path=$file["tmp_name"];
-  $filename=$file["name"];
-  $filetype=$file["type"];
-  echo "PATH:$server_path<br>";
-  echo "FILE:$filename<br>";
-  echo "TYPE:$filetype<br>";
-  insertFile("$filetype","$server_path",$FOLDERS["avatares"],"$filename");
-}
-
+        return $createdFile;
+      } catch (Exception $e) {
+        print "An error occurred: " . $e->getMessage();
+      }
+  }
+  //testing
+  if(isset($_FILES["foto"])){
+    $file=$_FILES["foto"];
+    $server_path=$file["tmp_name"];
+    $filename=$file["name"];
+    $filetype=$file["type"];
+    echo "PATH:$server_path<br>";
+    echo "FILE:$filename<br>";
+    echo "TYPE:$filetype<br>";
+    insertFile("$filetype","$server_path",$FOLDERS["avatares"],"$filename");
+  }
+?>
 
