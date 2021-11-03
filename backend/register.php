128724c1b830893891d8bbebc1189a59799a99e1
Chipskein <abfn0905@gmail.com>
2021-11-03 10:49:03 -0300

    send_mail() feito

--- backend/register.php
+++ backend/register.php
@@ -1,104 +1,54 @@
+
 <?php
-
-/**
- * This example shows how to send via Google's Gmail servers using XOAUTH2 authentication.
- */
-
-//Import PHPMailer classes into the global namespace
+require '../vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\OAuth;
-//Alias the League Google OAuth2 provider class
 use League\OAuth2\Client\Provider\Google;
-
-//SMTP needs accurate times, and the PHP time zone MUST be set
-//This should be done in your php.ini, but this is how to do it if you don't have access to that
 date_default_timezone_set('Etc/UTC');
-
-//Load dependencies from composer
-//If this causes an error, run 'composer install'
-require '../vendor/autoload.php';
-
-//Create a new PHPMailer instance
-$mail = new PHPMailer();
-
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
-
-//Enable SMTP debugging
-//SMTP::DEBUG_OFF = off (for production use)
-//SMTP::DEBUG_CLIENT = client messages
-//SMTP::DEBUG_SERVER = client and server messages
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-//Set the hostname of the mail server
-$mail->Host = 'smtp.gmail.com';
-
-//Set the SMTP port number:
-// - 465 for SMTP with implicit TLS, a.k.a. RFC8314 SMTPS or
-// - 587 for SMTP+STARTTLS
-$mail->Port = 465;
-
-//Set the encryption mechanism to use:
-// - SMTPS (implicit TLS on port 465) or
-// - STARTTLS (explicit TLS on port 587)
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
-
-//Set AuthType to use XOAUTH2
-$mail->AuthType = 'XOAUTH2';
-
-//Fill in authentication details here
-//Either the gmail account owner, or the user that gave consent
-$email = "dagama.ifrs@gmail.com";
-$clientId = '971777937005-dqnkd7sef0410teq7etqnl5es07ocha5.apps.googleusercontent.com';
-$clientSecret = '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';
-//Obtained by configuring and running get_oauth_token.php
-//after setting up an app in Google Developer Console.
-$refreshToken = '1//0h8NFG9y7CvcTCgYIARAAGBESNwF-L9Ir7YVXsCFyOGedjirP9o5GdAanrM21VsBzA-4FS2pbjMsBHfXE6eaR20R3wLwpDeR2B9Q';
-
-//Create a new OAuth2 provider instance
-$provider = new Google(
-    [
-        'clientId' => $clientId,
-        'clientSecret' => $clientSecret,
-    ]
-);
-
-//Pass the OAuth provider instance to PHPMailer
-$mail->setOAuth(
-    new OAuth(
+function send_mail($addr_mail){
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 465;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->SMTPAuth = true;
+    $mail->AuthType = 'XOAUTH2';
+    $email = "dagama.ifrs@gmail.com";
+    $clientId = '971777937005-dqnkd7sef0410teq7etqnl5es07ocha5.apps.googleusercontent.com';
+    $clientSecret = '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';
+    $refreshToken = '1//0h8NFG9y7CvcTCgYIARAAGBESNwF-L9Ir7YVXsCFyOGedjirP9o5GdAanrM21VsBzA-4FS2pbjMsBHfXE6eaR20R3wLwpDeR2B9Q';
+    $provider = new Google(
         [
-            'provider' => $provider,
             'clientId' => $clientId,
             'clientSecret' => $clientSecret,
-            'refreshToken' => $refreshToken,
-            'userName' => $email,
         ]
-    )
-);
-
-//Set who the message is to be sent from
-//For gmail, this generally needs to be the same as the user you logged in as
-$mail->setFrom($email);
-//Set who the message is to be sent to
-$mail->addAddress('abfn0905@gmail.com');
-
-//Set the subject line
-
-$mail->isHTML(true);                                  //Set email format to HTML
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-//send the message, check for errors
-if (!$mail->send()) {
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message sent!';
+    );
+    $mail->setOAuth(
+        new OAuth(
+            [
+                'provider' => $provider,
+                'clientId' => $clientId,
+                'clientSecret' => $clientSecret,
+                'refreshToken' => $refreshToken,
+                'userName' => $email,
+            ]
+        )
+    );
+    $mail->setFrom($email);
+    $mail->addAddress($addr_mail);
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    if (!$mail->send()) {
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Message sent!';
+    }
 }
+
 
 
 
