e9e8f8a924811b61445fb88800a39573b6d9ff89
Chipskein <abfn0905@gmail.com>
2021-11-03 22:35:30 -0300

    testando concexao

--- backend/infra/connection.php
+++ backend/infra/connection.php
@@ -1,17 +1,34 @@
 <?php
-    //connect with SQLite
-    if(preg_match("/localhost/","$_SERVER[HTTP_HOST]")){
-        echo "Connect with sqlite<br>";
-        $db=new SQLite3("");
-        $db->exec("");
-    }
-    if(preg_match("/dagama.herokuapp/","$_SERVER[HTTP_HOST]")){
-        echo "Connect with postgresql<br>";
-        $db=pg_connect($DATABASE_URL);
-        echo $db;
-    }
+    //connect with SQLit
     if(!preg_match("/localhost/","$_SERVER[HTTP_HOST]")&&!preg_match("/dagama.herokuapp/","$_SERVER[HTTP_HOST]")){
         echo "<h2>Inválido</h2>";
+    }
+    else{
+        $db=false;
+        if(preg_match("/localhost/","$_SERVER[HTTP_HOST]")){
+            echo "Connect with sqlite<br>";
+            //$db=new SQLite3("");
+            //$db->exec("");
+        }
+        if(preg_match("/dagama.herokuapp/","$_SERVER[HTTP_HOST]")){
+            echo "Connect with postgresql<br>";
+            $db_connect=pg_connect($DATABASE_URL);
+            if (!$db_connect) {
+                echo "An error occurred.\n";
+                exit;
+              }
+              
+              $result = pg_query($db_connect, "SELECT * FROM teste");
+              if (!$result) {
+                echo "An error occurred.\n";
+                exit;
+              }
+              
+              while ($row = pg_fetch_row($result)) {
+                echo $row['teste'];
+                echo "<br />\n";
+              }
+        }
     }
     
 ?>
