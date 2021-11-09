be04092fe2a918ca30614baea81c01fa003111fe
Chipskein <abfn0905@gmail.com>
2021-11-09 13:17:27 -0300

    getData funcionando  local e no heroku

--- backend/infra/dagama-psql.sql
+++ backend/infra/dagama-psql.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS INTERACAO_ASSUNTO ;
+DROP TABLE IF EXISTS INTERACAO_ASSUNTO;
 DROP TABLE IF EXISTS INTERACAO;
 DROP TABLE IF EXISTS CITACAOPERFIL;
 DROP TABLE IF EXISTS CITACAO;
@@ -12,39 +12,38 @@ DROP TABLE IF EXISTS PERFIL;
 DROP TABLE IF EXISTS ASSUNTO;
 DROP TABLE IF EXISTS PAIS;
 
---Alterar todas INTEGER --> SERIAL caso for Postgresql
 CREATE TABLE PAIS(
-    codigo  INTEGER NOT NULL,
+    codigo  SERIAL NOT NULL,
     nome    VARCHAR(250) NOT NULL,
     PRIMARY KEY(codigo)
 );
 CREATE TABLE ASSUNTO(
-    codigo  INTEGER NOT NULL,
+    codigo  SERIAL NOT NULL,
     nome    VARCHAR(250) NOT NULL,
     PRIMARY KEY(codigo)
 );
 CREATE TABLE PERFIL(
-    codigo INTEGER NOT NULL,
+    codigo SERIAL NOT NULL,
     pais INTEGER NOT NULL,
     email VARCHAR(250) NOT NULL UNIQUE,
     senha VARCHAR(250) NOT NULL,
     genero CHAR(1) CHECK(genero='M' OR genero='F' OR genero is null),
     username VARCHAR(250) NOT NULL,
     img VARCHAR(250) NOT NULL DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/4/4a/Pirate_icon.gif',
-    datanasc DATETIME NOT NULL,
-    dataregis DATETIME DEFAULT CURRENT_TIMESTAMP,
-    ativo BOOLEAN NOT NULL DEFAULT 0 CHECK(ativo=1 OR ativo=0),
+    datanasc TIMESTAMP NOT NULL,
+    dataregis TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ativo BOOLEAN NOT NULL DEFAULT false CHECK(ativo=true OR ativo=false),
     PRIMARY KEY(codigo),
     FOREIGN KEY (pais) REFERENCES PAIS(codigo)
 );
 --https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg
 CREATE TABLE PORTO(
-    codigo INTEGER NOT NULL,
+    codigo SERIAL NOT NULL,
     perfil INTEGER NOT NULL,
     nome VARCHAR(250) NOT NULL,
     descr VARCHAR(250) NOT NULL,
     img VARCHAR(250) NOT NULL DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg',
-    ativo BOOLEAN NOT NULL DEFAULT 1 CHECK(ativo=1 OR ativo=0),
+    ativo BOOLEAN NOT NULL DEFAULT true CHECK(ativo=true OR ativo=false),
     PRIMARY KEY(codigo),
     FOREIGN KEY (perfil) REFERENCES PERFIL(codigo) 
 );
@@ -55,49 +54,49 @@ CREATE TABLE PORTO_PARTICIPA(
     FOREIGN KEY (perfil) REFERENCES PERFIL(codigo)
 );
 CREATE TABLE SELO(
-    codigo INTEGER NOT NULL,
+    codigo SERIAL NOT NULL,
     porto INTEGER NOT NULL,
     texto VARCHAR(250) NOT NULL,
     img VARCHAR(250),
-    ativo BOOLEAN NOT NULL DEFAULT 1 CHECK(ativo=1 OR ativo=0),
+    ativo BOOLEAN NOT NULL DEFAULT true CHECK(ativo=true OR ativo=false),
     PRIMARY KEY(codigo),
     FOREIGN KEY (porto) REFERENCES PORTO(codigo)
 );
 CREATE TABLE SELOUSER(
     selo INTEGER NOT NULL,
     perfil INTEGER NOT NULL,
-    dateVal DATETIME NOT NULL,
+    dateVal TIMESTAMP NOT NULL,
     FOREIGN KEY (selo) REFERENCES SELO(codigo),
     FOREIGN KEY (perfil) REFERENCES PERFIL(codigo)
 );
 CREATE TABLE AMIGO(
     amigo INTEGER NOT NULL CHECK(amigo!=perfil),
     perfil INTEGER NOT NULL CHECK(amigo!=perfil),
-    dateAceito DATETIME NOT NULL,
-    ativo BOOLEAN NOT NULL DEFAULT 0 CHECK(ativo=1 OR ativo=0),
+    dateAceito TIMESTAMP NOT NULL,
+    ativo BOOLEAN NOT NULL DEFAULT false CHECK(ativo=true OR ativo=false),
     FOREIGN KEY (amigo) REFERENCES PERFIL(codigo),
     FOREIGN KEY (perfil) REFERENCES PERFIL(codigo)
 );
 CREATE TABLE SOLICITACAO_AMIGO(
     amigo INTEGER NOT NULL CHECK(amigo!=perfil),
     perfil INTEGER NOT NULL CHECK(amigo!=perfil),
-    dateEnvio DATETIME NOT NULL,
-    ativo BOOLEAN NOT NULL DEFAULT 1 CHECK(ativo=1 OR ativo=0),
+    dateEnvio TIMESTAMP NOT NULL,
+    ativo BOOLEAN NOT NULL DEFAULT true CHECK(ativo=true OR ativo=false),
     FOREIGN KEY (amigo) REFERENCES PERFIL(codigo),
     FOREIGN KEY (perfil) REFERENCES PERFIL(codigo)
 );
 CREATE TABLE INTERACAO(
-    codigo INTEGER NOT NULL,
+    codigo SERIAL NOT NULL,
     perfil INTEGER NOT NULL,
     perfil_posting INTEGER CHECK((post IS NULL AND porto IS NULL) OR perfil_posting IS NULL),
     porto INTEGER CHECK((post IS NULL AND perfil_posting IS NULL) OR porto IS NULL),
     post INTEGER CHECK((porto IS NULL AND perfil_posting IS NULL) OR post IS NULL),
-    data DATETIME DEFAULT CURRENT_TIMESTAMP,
+    data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     texto VARCHAR(250) NOT NULL,
     isReaction BOOLEAN CHECK((post IS NOT NULL AND isSharing IS NULL) OR isReaction IS NULL),
     isSharing BOOLEAN  CHECK((post IS NOT NULL AND isReaction IS NULL) OR isSharing IS NULL),
     emote VARCHAR(250) CHECK(isReaction IS NOT NULL OR emote IS NULL),
-    ativo BOOLEAN NOT NULL DEFAULT 1 CHECK(ativo=1 OR ativo=0),
+    ativo BOOLEAN NOT NULL DEFAULT true CHECK(ativo=true OR ativo=false),
     PRIMARY KEY(codigo),
     FOREIGN KEY (perfil) REFERENCES PERFIL(codigo),
     FOREIGN KEY (perfil_posting) REFERENCES PERFIL(codigo),
@@ -105,9 +104,9 @@ CREATE TABLE INTERACAO(
     FOREIGN KEY (post) REFERENCES INTERACAO(codigo)
 );
 CREATE TABLE CITACAO(
-    codigo INTEGER NOT NULL,
+    codigo SERIAL NOT NULL,
     interacao INTEGER NOT NULL,
-    ativo BOOLEAN NOT NULL DEFAULT 1 CHECK(ativo=1 OR ativo=0),
+    ativo BOOLEAN NOT NULL DEFAULT true CHECK(ativo=true OR ativo=false),
     PRIMARY KEY(codigo),
     FOREIGN KEY (interacao) REFERENCES INTERACAO(codigo)
 );
@@ -134,9 +133,9 @@ INSERT INTO PORTO(perfil,nome,descr) VALUES(1,'PORTO DE TESTE','è isso ai parce
 INSERT INTO PORTO(perfil,nome,descr) VALUES(1,'PORTO DE TESTE2','è isso ai parceria2');
 INSERT INTO PORTO_PARTICIPA(porto,perfil) VALUES(1,1),(2,1),(1,2);
 --amigos
-INSERT INTO SOLICITACAO_AMIGO(perfil,amigo,dateEnvio,ativo) VALUES(1,2,CURRENT_TIMESTAMP,0);
-INSERT INTO AMIGO(perfil,amigo,dateAceito,ativo) VALUES(1,2,CURRENT_TIMESTAMP,1);
-UPDATE SOLICITACAO_AMIGO SET ATIVO=0 WHERE PERFIL=1 AND AMIGO=2;
+INSERT INTO SOLICITACAO_AMIGO(perfil,amigo,dateEnvio,ativo) VALUES(1,2,CURRENT_TIMESTAMP,false);
+INSERT INTO AMIGO(perfil,amigo,dateAceito,ativo) VALUES(1,2,CURRENT_TIMESTAMP,true);
+UPDATE SOLICITACAO_AMIGO SET ATIVO=false WHERE PERFIL=1 AND AMIGO=2;
 --selos
 INSERT INTO SELO(porto,texto) VALUES(1,'SELO DE TESTE');
 INSERT INTO SELO(porto,texto) VALUES(1,'SELO DE TESTE2');
@@ -158,9 +157,9 @@ INSERT INTO INTERACAO(perfil,perfil_posting,texto) values
 ;
 --REAÇÕES
 INSERT INTO INTERACAO(perfil,post,texto,isReaction,emote) VALUES
-(1,6,'Like',1,'https://en.wikipedia.org/wiki/Facebook_like_button#/media/File:Facebook_Thumb_icon.svg');
+(1,6,'Like',true,'https://en.wikipedia.org/wiki/Facebook_like_button#/media/File:Facebook_Thumb_icon.svg');
 --COMPARTILHAMENTO
-INSERT INTO INTERACAO(perfil,post,texto,isSharing) VALUES(1,2,'compartilhou ',1);
+INSERT INTO INTERACAO(perfil,post,texto,isSharing) VALUES(1,2,'compartilhou ',true);
 
 
 
