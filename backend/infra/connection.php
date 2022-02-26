5d4e41e61fcef6ae20122d4a73f1f080ec16eb15
Chipskein <abfn0905@gmail.com>
2022-02-26 12:23:49 -0300

    reactor: ./ -> public/

--- backend/infra/connection.php
+++ backend/infra/connection.php
@@ -1,14 +1,22 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT'].'/backend/mailer/mailer.php';
     include $_SERVER['DOCUMENT_ROOT'].'/backend/gdrive/driver.php';
+    
+    $dotenv_dir=$_SERVER['DOCUMENT_ROOT'].'/backend';
+    $dotenv = Dotenv\Dotenv::createImmutable($dotenv_dir, '.env');
+    $dotenv->load();
+    
+    
+    
+    
+    
+    
     function db_connection(){
         $db=false;
         $db_type=false;
         if(preg_match("/localhost/","$_SERVER[HTTP_HOST]")){
             //echo "Connect with sqlite<br>";
             $db=new SQLite3("$_SERVER[DOCUMENT_ROOT]/backend/infra/dagama.db");
-            // $sql = file_get_contents("$_SERVER[DOCUMENT_ROOT]/backend/infra/dagama.sql");
-            // $qr = $db->exec($sql);
             $test=$db->exec("PRAGMA FOREIGN_KEYS=ON");
             if(!$test){
                 echo "<br>Um erro de conexão com banco ocorreu<br>"; 
@@ -16,14 +24,6 @@
             }
             $db_type='sqlite';
         }
-        // if(preg_match("/dagama.herokuapp/","$_SERVER[HTTP_HOST]")){
-        //     $db=pg_connect(getenv("DATABASE_URL"));
-        //     if (!$db) {
-        //         echo "<br>Um erro de conexão com banco ocorreu<br>"; 
-        //         exit;
-        //     }
-        //     $db_type='postgresql';
-        // }
         return ['db'=>$db,'db_type'=>$db_type];
     }
 
