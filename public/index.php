c5625fc4d78d81fcfad6ebed702358c7b2d6cb74
Chipskein <abfn0905@gmail.com>
2022-02-26 15:42:01 -0300

    testing

--- public/index.php
+++ public/index.php
@@ -52,7 +52,7 @@
     <div id="loginContainer">
       <p class="headerTxt">Login</p>
       <div class="divLogin">
-        <form action="/app/backend/login.php" method="post" id="form" name="form">
+        <form action="../backend/login.php" method="post" id="form" name="form">
           <div class="containerLogin">
             <input class="inputs" name="email" type="email" placeholder="Email">
             <input class="inputs" name="password" type="password" placeholder='Senha'>
@@ -74,7 +74,7 @@
         <p class="headerTxt">Cadastro</p>
       </div>
       <div class="divRegister">
-        <form action="/app/backend/register.php" method="post" id="formRegister" name="formRegister" enctype="multipart/form-data">
+        <form action="../backend/register.php" method="post" id="formRegister" name="formRegister" enctype="multipart/form-data">
           <div class="containerRegister">
             <div id="imgs_register">
                 <div id="img_input1">
